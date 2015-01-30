c9f854c3a2c71abae0effe388b891064c7dcb55e
Lukáš Doležal <dolezal@itlab.cz>
2015-01-30 18:39:38 +0100

    main require file

---
+++ lib/rails_api_kit.rb
@@ -0,0 +1,4 @@
+require 'rails_api_kit/policy_scoped_attributes_serializer'
+require 'rails_api_kit/request_scoped_attributes_serializer'
+require 'rails_api_kit/fields_serialization_scope'
+require 'rails_api_kit/fields_serialization_scope_controller_module'
